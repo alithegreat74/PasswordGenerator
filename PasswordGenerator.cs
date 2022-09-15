b1631fe094cadb0db376d4221e7af292ae455054
Alireza Mohammadi <am1514168021@gmail.com>
2022-09-15 01:41:49 -0700

    generator complete

--- PasswordGenerator.cs
+++ PasswordGenerator.cs
@@ -10,7 +10,7 @@ namespace PasswordGenerator
     {
         static void generate()
         {
-            int rngPossibility;
+            
             int characters;
             Console.Write("How many character does your password have?\nIf you want to return, enter 0 you loser. ");
             Int32.TryParse(Console.ReadLine(), out characters);
@@ -20,7 +20,7 @@ namespace PasswordGenerator
                 Main();
             }
             char[] Password = new char[characters];
-            Console.Write("How should your password be?\n1)Only numbers\n2)Numbers + letters\n3)Numbers + letters + signs\nChoose: ");
+            Console.Write("How should your password be?\n1)Only numbers\n2)Numbers + letters\n3)Numbers + letters + symbols\nChoose: ");
             int choice;
             Int32.TryParse(Console.ReadLine(), out choice);
             while(choice!=1 && choice!=2 && choice!=3)
@@ -35,41 +35,66 @@ namespace PasswordGenerator
             }
             int rng=0;
             Random rnd=new Random();
+            int[] symbols = new int[33] { 32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,58,59,60,61,62,63,64,91,92,93,94,95,96,123,124,125,126 } ;
+            
             if (choice==1)
             {
                 
                 for (int i = 0; i<characters; i++)
                 {
                     Password[i]=Convert.ToChar(rnd.Next(48, 57));
-                }
-                for (int i = 0; i<characters; i++)
-                {
                     Console.Write(Password[i]);
                 }
-                    
+                Console.Write("\n");
             }
             else if (choice==2)
             {
                 for(int i = 0; i<characters; i++)
                 {
-                    rng=rnd.Next(1, 2);
-                    if (rng==1)
+                    rng=rnd.Next(1, 20);
+                    if (rng<10)
                     {
                         Password[i]=Convert.ToChar(rnd.Next(48, 57));
                     }
-                    else if (rng==2)
+                    else if (rng<15)
                     {
-                        Password[i]=(char)rnd.Next(65, 122);
+                        Password[i]=(char)rnd.Next(65, 90);
                     }
-                }
-                for (int i = 0; i<characters; i++)
-                {
+                    else if (rng<20)
+                    {
+                        Password[i]=(char)rnd.Next(97, 122);
+                    }
                     Console.Write(Password[i]);
                 }
+                Console.Write("\n");
             }
+            
+            
             else if (choice==3)
             {
-
+                for(int i = 0; i<characters; i++)
+                {
+                    rng=rnd.Next(1, 30);
+                    if (rng<10)
+                    {
+                        Password[i]=Convert.ToChar(rnd.Next(48, 57));
+                    }
+                    else if (rng<15)
+                    {
+                        Password[i]=(char)rnd.Next(65, 90);
+                    }
+                    else if (rng<20)
+                    {
+                        Password[i]=(char)rnd.Next(97, 122);
+                    }
+                    else if (rng<30)
+                    {
+                        int pass = rnd.Next(symbols.Length);
+                        Password[i]=(char)symbols[pass+1];
+                    }
+                    Console.Write(Password[i]);
+                }
+                Console.Write("\n");
             }
         }
         static void check()
